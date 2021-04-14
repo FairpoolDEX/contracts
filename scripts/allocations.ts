4f4676bd6b907333a94886a9a29df05824c64f56
idealatom <idealatom@gmail.com>
2021-04-14 19:57:21 +0700

    add deploy script with adding allocations

---
+++ scripts/allocations.ts
@@ -0,0 +1,13 @@
+// it's a test only allocations with a random addresses!
+const ALLOCATIONS: {[index: string]: {[index: string]: number}} = {
+  '0': {
+    '0xC30C915dE5FC456F00BaFea00b8fF2a24b3b384d': 100,
+    '0x77BD3E7f5b353834EB93CF8076e2500BD2ADBff1': 20,
+    '0x3a10757948BeAeA4e0D76bF7adc676A17E35ACc5': 400,
+  },
+  '1': {
+    '0x62baee26eB52E0D43ed003017833E7701C2e037B': 100,
+    '0xAbAE711bC28b21D91bC473191A379F548Cb183ba': 20,
+  },
+}
+export default ALLOCATIONS
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,10 +1,23 @@
-import { ethers } from "hardhat"
+import { ethers, upgrades } from "hardhat"
+
+import ALLOCATIONS from './allocations'
+
 
 async function main() {
-  const Greeter = await ethers.getContractFactory("Greeter")
-  const greeter = await Greeter.deploy("hai")
-  await greeter.deployed()
-  console.log("Greeter address:", greeter.address) // eslint-disable-line no-console
+  const ShieldToken = await ethers.getContractFactory("ShieldToken")
+  const token = await upgrades.deployProxy(ShieldToken)
+  await token.deployed()
+
+  console.log("Token address:", token.address) // eslint-disable-line no-console
+
+  for (const vestingTypeIndex of Object.keys(ALLOCATIONS)) {
+    const addresses = Object.keys(ALLOCATIONS[vestingTypeIndex])
+    const amounts = Object.values(ALLOCATIONS[vestingTypeIndex])
+    await token.addAllocations(addresses, amounts, vestingTypeIndex)
+
+    console.log(`Added allocation of type ${vestingTypeIndex} for ${addresses.length} addresses`) // eslint-disable-line no-console
+  }
+
 }
 
 main()
