6b5a80cf90af23cb53883411327771f484777326
Petr Korolev <sky4winder@gmail.com>
2021-04-16 19:45:02 +0700

    fill deploy scripts with template and comments

--- scripts/parameters.ts
+++ scripts/parameters.ts
@@ -2,13 +2,56 @@ export const RELEASE_TIME: number = Math.floor(new Date("2021.04.21 15:00:00 GMT
 
 // it's a test only allocations with a random addresses!
 export const ALLOCATIONS: {[index: string]: {[index: string]: number}} = {
+
+// Seed:	Locked for 1 month, 5% on first release, then equal parts of 12% over total of 9 months
   '0': {
     '0xC30C915dE5FC456F00BaFea00b8fF2a24b3b384d': 100,
     '0x77BD3E7f5b353834EB93CF8076e2500BD2ADBff1': 20,
     '0x3a10757948BeAeA4e0D76bF7adc676A17E35ACc5': 400,
   },
+// Private:	10% at listing, then equal parts of 18% over total of 6 months
   '1': {
     '0x62baee26eB52E0D43ed003017833E7701C2e037B': 100,
     '0xAbAE711bC28b21D91bC473191A379F548Cb183ba': 20,
+  },
+// Public:	100% at listing
+  '2': {
+    '0xdeadbeef': 77,
+    '0x1337': 42,
+  },
+// Advisors, Partners:	Locked for 1 month, 4% on first release, then equal parts of 4% over total of 24 months
+  '3': {
+    '0xdeadbeef': 77,
+    '0x1337': 42,
+  },
+// Team:	Locked for 12 months, 8% on first release, then equal parts of 8% over total of 12 months
+  '4': {
+    '0xdeadbeef': 77,
+    '0x1337': 42,
+  },
+// Development:	Locked for 6 months, 3% on first release, then equal parts of 3% over total of 36 months
+  '5': {
+    '0xdeadbeef': 77,
+    '0x1337': 42,
+  },
+// Marketing:	Locked for 3 months, 2% on first release, then equal parts of 2% over total of 48 months
+  '6': {
+    '0xdeadbeef': 77,
+    '0x1337': 42,
+  },
+// Liquidity provisioning:	100% at listing
+  '7': {
+    '0xdeadbeef': 77,
+    '0x1337': 42,
+  },
+// Liquidity mining:	8% at listing, then equal parts of 8% over total of 12 months
+  '8': {
+    '0xdeadbeef': 77,
+    '0x1337': 42,
+  },
+// General Reserve:	Locked for 6 months, 2% on first release, then equal parts of 2% over total of 60 months
+  '9': {
+    '0xdeadbeef': 77,
+    '0x1337': 42,
   },
 }
