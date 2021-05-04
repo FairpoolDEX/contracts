047b964ba0b854154aa044c173c2d44249bb7cc4
idealatom <idealatom@gmail.com>
2021-05-04 16:12:10 +0700

    clean comments in hardhat.config.js

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,18 +3,11 @@ import "@nomiclabs/hardhat-waffle"
 import "@nomiclabs/hardhat-etherscan"
 import "hardhat-watcher"
 import "hardhat-typechain"
-// import "hardhat-deploy"
-// import 'hardhat-deploy-ethers'
 import "solidity-coverage"
 import "@openzeppelin/hardhat-upgrades"
 import { config as dotEnvConfig } from "dotenv"
 
 dotEnvConfig()
-
-// const INFURA_API_KEY = process.env.INFURA_API_KEY || ""
-// const ROPSTEN_PRIVATE_KEY =
-//   process.env.ROPSTEN_PRIVATE_KEY ||
-//   "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3" // well known private key
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -40,9 +33,6 @@ const config: HardhatUserConfig = {
       },
       timeout: 100000,
     },
-    // coverage: {
-    //   url: "http://127.0.0.1:8555",
-    // },
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
