dcbc195bea8a50c71d69424aad1dfd91391805b3
idealatom <idealatom@gmail.com>
2021-05-04 14:43:41 +0700

    update networks in hardhat.config.js

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,11 +11,10 @@ import { config as dotEnvConfig } from "dotenv"
 
 dotEnvConfig()
 
-const INFURA_API_KEY = process.env.INFURA_API_KEY || ""
-const ROPSTEN_PRIVATE_KEY =
-  process.env.ROPSTEN_PRIVATE_KEY ||
-  "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3" // well known private key
-const { ETHERSCAN_API_KEY } = process.env
+// const INFURA_API_KEY = process.env.INFURA_API_KEY || ""
+// const ROPSTEN_PRIVATE_KEY =
+//   process.env.ROPSTEN_PRIVATE_KEY ||
+//   "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3" // well known private key
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -27,16 +26,24 @@ const config: HardhatUserConfig = {
     ],
   },
   networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "",
+      },
+    },
     ropsten: {
-      url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [ROPSTEN_PRIVATE_KEY],
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "",
+      },
     },
     // coverage: {
     //   url: "http://127.0.0.1:8555",
     // },
   },
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
   watcher: {
     run: {
