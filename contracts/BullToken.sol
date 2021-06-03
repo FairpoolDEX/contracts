046ac3b3fcfd6d0724aae1a3f78910c7a2a96b10
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2021-06-03 15:03:30 +0700

    Audit via MythX

--- contracts/BullToken.sol
+++ contracts/BullToken.sol
@@ -6,6 +6,9 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "hardhat/console.sol";
 
+/* mythx-disable SWC-103 */
+/* mythx-disable SWC-116 */
+
 contract BullToken is OwnableUpgradeable, ERC20PausableUpgradeable {
     mapping(address => uint) public claims;
     uint public airdropStartTimestamp;
