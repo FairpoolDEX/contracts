a85340798b1ddd7ccad777120d5466a7c9458bbc
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2021-06-06 11:50:43 +0700

    Minor fixes

--- tasks/claimBullToken.ts
+++ tasks/claimBullToken.ts
@@ -2,7 +2,6 @@ import fs from "fs"
 import { without } from "lodash"
 import type { ethers } from "ethers"
 import { HardhatRuntimeEnvironment, TaskArguments } from "hardhat/types"
-import { BullToken } from "../typechain"
 import { HardhatEthersHelpers } from "@nomiclabs/hardhat-ethers/types"
 
 export type Key = string;
@@ -13,7 +12,7 @@ export async function parseKeys(data: Buffer | string): Promise<Keys> {
   return without(data.toString().split("\n"), '')
 }
 
-export async function claimBullToken(token: BullToken, keys: Keys, ethers: Ethers, log: ((msg: any) => void) | void): Promise<void> {
+export async function claimBullToken(token: any, keys: Keys, ethers: Ethers, log: ((msg: any) => void) | void): Promise<void> {
   for (const key of keys) {
     const wallet = new ethers.Wallet(Buffer.from(key, "hex"))
     const signer = wallet.connect(ethers.provider)
@@ -37,7 +36,7 @@ export async function claimBullTokenTask(args: TaskArguments, hre: HardhatRuntim
   console.log(`[INFO] Reading private keys from ${keysPath}`)
   const keys = await parseKeys(fs.readFileSync(keysPath))
   console.log(`[INFO] Attaching to contract ${tokenAddress}`)
-  const Token = await ethers.getContractFactory("BullToken") as unknown as BullToken
+  const Token = await ethers.getContractFactory("BullToken")
   const token = await Token.attach(tokenAddress)
   console.log(`[INFO] Claiming $BULL`)
   await claimBullToken(token, keys, ethers, console.log.bind(console))
