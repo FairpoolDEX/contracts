d2be3c7ec6d521b6543dc54e1efb20d719913539
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2021-08-06 10:37:42 +0700

    Update transferManyShieldToken

--- tasks/transferManyShieldToken.ts
+++ tasks/transferManyShieldToken.ts
@@ -1,6 +1,11 @@
 import { HardhatRuntimeEnvironment, TaskArguments } from "hardhat/types"
 import { Allocations } from "../types"
 import { chunk } from "../test/support/all.helpers"
+import { BigNumber } from "ethers"
+import { expect } from "../util/expect"
+import { shieldMaxSupplyTokenAmount } from "../test/support/ShieldToken.helpers"
+
+// NOTE: Amounts should be with decimals (with a dot)
 
 export async function transferManyShieldToken(args: TaskArguments, hre: HardhatRuntimeEnvironment): Promise<void> {
   const allocations: Allocations = (await import(args.allocations)).default
@@ -13,6 +18,8 @@ export async function transferManyShieldToken(args: TaskArguments, hre: HardhatR
 
   const recipients = Object.keys(allocation)
   const amounts = Object.values(allocation).map(i => hre.ethers.utils.parseUnits(i, "18"))
+  const totalAmounts = amounts.reduce((acc, amount) => acc.add(amount), BigNumber.from(0))
+  expect(totalAmounts.lt(shieldMaxSupplyTokenAmount)).to.be.true
 
   console.log(`Calling transferMany with ${recipients.length} recipients and chunk size ${args.chunk}:`)
 
@@ -29,7 +36,7 @@ export async function transferManyShieldToken(args: TaskArguments, hre: HardhatR
     const allocationChunk = chunkedRecipients[i].reduce((obj, address, index) => ({ ...obj, [address]: chunkedAmounts[i][index].toString() }), {})
     console.log(allocationChunk)
 
-    const tx = await token.transferMany(chunkedRecipients[i], chunkedAmounts[i], { gasLimit: 2500000 })
+    const tx = await token.transferMany(chunkedRecipients[i], chunkedAmounts[i], { type: 2, gasLimit: 2500000 })
     console.log(`TX Hash: ${tx.hash}`)
   }
 }
