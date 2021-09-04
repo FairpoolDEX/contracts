f61a151c02084fe7880a1adb4f73e9d530cc9cd3
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2021-09-04 17:32:35 +0700

    Reduce contract size, fix deployContract

---
+++ tasks/arguments/TraderLoot.arguments.ts
@@ -0,0 +1,3 @@
+import { maxClaimTimestamp, name, ownerMaxTokenId, publicMaxTokenId, style, symbol } from "../../test/support/TraderLoot.helpers"
+
+export default [name, symbol, "0xd49EFA7BC0D339D74f487959C573d518BA3F8437", publicMaxTokenId, ownerMaxTokenId, maxClaimTimestamp, style]
