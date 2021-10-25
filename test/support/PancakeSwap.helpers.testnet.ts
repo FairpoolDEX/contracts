c02bdd858a28a14b4ca410dbbaf0ab753374b009
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2021-10-25 14:14:09 +0700

    Add PancakeSwap testnet contracts

--- test/support/PancakeSwap.helpers.testnet.ts
+++ test/support/PancakeSwap.helpers.testnet.ts
@@ -1,6 +1,8 @@
-export const router = "0xD99D1c33F9fC3444f8101754aBC46c52416550D1"
+// https://pancake.kiemtienonline360.com/#/pool
 
-export const factory = "0x6725F303b657a9451d8BA641348b6761A6CC7a17"
+export const router = "0x4d09374541c8beecd288d8d126457d11a5b3dc8c"
 
-export const WETH = "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd"
-// It's WBNB actually
+export const factory = "0xb7926c0430afb07aa7defde6da862ae0bde767bc"
+
+// WBNB actually
+export const WETH = "0xae13d989dac2f0debff460ac112a837c89baa7cd"
