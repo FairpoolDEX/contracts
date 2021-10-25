b92f22686b2e449b817f27708d9d1558e4db37b2
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2021-10-25 14:52:37 +0700

    Fix PancakeSwap routr

--- test/support/PancakeSwap.helpers.testnet.ts
+++ test/support/PancakeSwap.helpers.testnet.ts
@@ -1,6 +1,6 @@
 // https://pancake.kiemtienonline360.com/#/pool
 
-export const router = "0x4d09374541c8beecd288d8d126457d11a5b3dc8c"
+export const router = "0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3"
 
 export const factory = "0xb7926c0430afb07aa7defde6da862ae0bde767bc"
 
