8e16033ec3736db54693f94cd0a3a550133e6948
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-01-20 20:18:02 +0700

    unwrapSmartContractBalanceAtBlockTag

--- models/ContractType.ts
+++ models/ContractType.ts
@@ -2,11 +2,13 @@ import { z } from 'zod'
 
 export const ContractTypeSchema = z.enum([
   'NFTrade',
-  'UniswapV2',
-  'UniswapV3',
+  'UniswapV2Pair',
+  'UniswapV3Pair',
   'TeamFinance',
+  'GnosisProxy',
+  'FlashWallet_ZeroExProxy', // 0x proxy
 ])
 
-export const { NFTrade, UniswapV2, UniswapV3, TeamFinance } = ContractTypeSchema.enum
+export const { NFTrade, UniswapV2Pair, UniswapV3Pair, TeamFinance } = ContractTypeSchema.enum
 
 export type ContractType = z.infer<typeof ContractTypeSchema>
