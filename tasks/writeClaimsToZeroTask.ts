a2e51560d85c28726d541b8e0b64700269ba1a36
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-03-24 18:33:19 +0700

    Note

--- tasks/writeClaimsToZeroTask.ts
+++ tasks/writeClaimsToZeroTask.ts
@@ -10,6 +10,8 @@ import { zero } from '../util/bignumber'
 export async function writeClaimsToZeroTask(args: WriteClaimsTaskArguments, hre: HardhatRuntimeEnvironment): Promise<void> {
   const context = await getWriteClaimsContext(args, hre)
   const { rewrites: rewritesPath, expectations: expectationsPath, out, log } = context
+  // NOTE: Next time execute setClaims manually to avoid changing the owner
+  // Contract URL: https://bscscan.com/address/0xEFB7311cc5d66b19FeD6a19148A3Ecf801e65100#writeProxyContract
   const exceptions = ['0x1ed55e06Ca8a6686eB06692676C267cF6Df9D642', '0x01CBb72a9eC45977Ae7A146cC7dbf97AEA31A201']
   const rewrites = await getRewritesFromCSVFile(rewritesPath)
   const validators = await importDefault(expectationsPath)
