1636e5a2b452ad90aae9ca37adeb3aaf69aaa42f
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-06-16 08:39:15 +0700

    addAllocationsTask

--- tasks/addAllocations.ts
+++ tasks/addAllocations.ts
@@ -8,8 +8,9 @@ import { getGenericTokenWithVesting } from './util/getToken'
 import { Address } from '../models/Address'
 import { logAndWaitForTransactions } from './util/transaction'
 import { concat, sortBy } from 'lodash'
-import { addUnvestedAllocations, getAllocations, setAllocations, splitAllocations } from '../test/support/Allocation.helpers'
+import { addVestedAllocations, getAllocations, splitAllocations } from '../test/support/Allocation.helpers'
 import { AmountBN } from '../models/AmountBN'
+import { transferManyBalanceBN } from '../test/support/TransferMany.helpers'
 
 export async function addAllocationsTask(args: AddAllocationsTaskArguments, hre: HardhatRuntimeEnvironment): Promise<void> {
   const context = await getAddAllocationsContext(args, hre)
@@ -18,9 +19,9 @@ export async function addAllocationsTask(args: AddAllocationsTaskArguments, hre:
   const vestingTypes = await getVestingTypes(context.vestingTypes)
   const allocations = await getAllocations(totalAmount, context.allocations)
   const { vested, unvested } = splitAllocations(allocations)
-  const vestedTransactions = await setAllocations(vestingTypes, token, vested)
-  const unvestedTransaction = await addUnvestedAllocations(token, unvested)
-  const allTransactions = concat(vestedTransactions, [unvestedTransaction])
+  const vestedTransactions = await addVestedAllocations(context, token, vestingTypes, vested)
+  const unvestedTransactions = await transferManyBalanceBN(context, token, unvested, 400)
+  const allTransactions = concat(vestedTransactions, unvestedTransactions)
   const minConfirmations = 1
   await logAndWaitForTransactions(context, minConfirmations, allTransactions)
 }
