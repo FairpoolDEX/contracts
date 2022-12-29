a2691179efcff80e990348ed572b01b54d54e356
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-12-29 14:30:40 +0700

    deployContractTask

--- fix-licenses.sh
+++ fix-licenses.sh
@@ -2,6 +2,7 @@
 
 if ! command -v gsed > /dev/null 2>&1; then
   echo "Install gsed via \`brew install gnu-sed\`"
+  exit 1
 fi
 
 find . -type f -name "*.sol" -exec sh -c 'if ! grep -q "SPDX-License-Identifier" "$0"; then gsed -i "1i //\ SPDX-License-Identifier:\ UNLICENSED" "$0"; fi' {} \;
---
+++ postinstall.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+lefthook install
+./fix-licenses.sh
