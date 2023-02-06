1d3fd33f24d577841127f9670cd57660dded50ae
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-02-06 16:03:11 +0700

    Remove solmate

--- tasks/arguments/Fairpool.all.arguments.ts
+++ tasks/arguments/Fairpool.all.arguments.ts
@@ -3,13 +3,17 @@ import { DefaultSlope, DefaultWeight } from '../../libs/fairpool/constants'
 import { getSharePercent } from '../../libs/fairpool/utils'
 import { getStringEnvVar } from '../../libs/utils/process'
 
+// const contract = await context.ethers.getContractAt('Fairpool', '0x3fb9576809E0e908b2E44BF4C191fe33f3121436')
+// await contract.setEarnings(150000)
+// return
+
 const args: Parameters<Fairpool__factory['deploy']> = [
   getStringEnvVar('NAME', process.env.NAME, 'Official Fairpool Token'),
   getStringEnvVar('SYMBOL', process.env.SYMBOL, 'FAIR'),
   DefaultSlope,
   DefaultWeight,
   getSharePercent(75, 1000),
-  getSharePercent(200, 1000),
+  getSharePercent(150, 1000),
   [],
   [],
 ]
